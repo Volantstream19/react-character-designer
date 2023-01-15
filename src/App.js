623fa2d5ab51f5696944a0b0cd13ef7be2cd7ab3
King Kong <king-kong@gmail.com>
2023-01-14 16:59:58 -0800

    singers bodies are displaying, counter is updating to. removed editor from app cause porblem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import Controls from './components/Controls/Controls.js';
-import Editor from './components/Editor/Editor.js';
 import Display from './components/Display/Display';
 
 function App() {
   return (
     <div className="App">
       <Display />
-      <Editor />
     </div>
   );
 }
--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -6,20 +6,30 @@ import './Display.css';
 export default function Display() {
   const [head, setHead] = useState('lizzo-head');
   const [headCount, headCountChanged] = useState(0);
+  const [body, setBody] = useState('clairo-body');
+  const [bodyCount, bodyCountChanged] = useState(0);
 
   const handleChange = (lig) => {
     if (lig === 'head') {
       headCountChanged((state) => state + 1);
+    } else if (lig === 'body') {
+      bodyCountChanged((state) => state + 1);
     }
   };
 
-  const changeCounter = `you have changed the head ${headCount} time(s)`;
+  const changeCounter = `You've changed the head ${headCount}, you've changed the body ${bodyCount} time(s)`;
 
   return (
     <div className="singer-select">
-      <Editor head={head} />
+      <Editor head={head} body={body} />
 
-      <Controls head={head} setHead={setHead} handleChange={handleChange} />
+      <Controls
+        head={head}
+        body={body}
+        handleChange={handleChange}
+        setHead={setHead}
+        setBody={setBody}
+      />
 
       <div className="change-counter">{changeCounter}</div>
     </div>
